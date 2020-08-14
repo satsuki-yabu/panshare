3e9d5e6e2945a35cd93aff9162975ba2ed463921
satsuki.yabu <satsuki.yabu@techacademy.jp>
2020-08-14 10:59:26 +0000

    improve

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,12 @@
-CarrierWave.configure do |config|
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-    config.fog_provider = 'fog/aws'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws' #←ここを追記
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION']
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory = ENV['S3_BUCKET'] 
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
-
-    
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
